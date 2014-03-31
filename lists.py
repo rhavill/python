abbb408260163026635bb61bc622d48a84efea3f
Ricardo Havill <rhavill@progresstesting.com>
2014-03-31 05:38:33 -0400

    Added some examples.

--- lists.py
+++ lists.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
+names = ["Adam","Alex","Mariah","Martine","Columbus"]
+for name in names:
+    print name
+
 inventory = {
     'gold' : 500,
     'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
     'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
     
 }
+
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+# Add your code below!
+for key in webster:
+    print webster[key]
 
 # Adding a key 'burlap bag' and assigning a list to it
 inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
@@ -19,3 +34,29 @@ inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold'] += 50
 print inventory
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total = total +  prices[item]
+            stock[item] = stock[item] - 1
+    return total
+print compute_bill(shopping_list)    
