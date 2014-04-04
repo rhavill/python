64edb53cc5c0350983f9cfbf74a5cb2380e01924
Ricardo Havill <rhavill@progresstesting.com>
2014-04-03 22:52:58 -0400

    Added string examples.

--- strings.py
+++ strings.py
@@ -7,3 +7,10 @@ print "HELLO"[0]
 print "This is %s. This is %s." % (my_string, a_string)
 
 print "hi " +str(3)
+
+def censor(text, word):
+    l = len(word)
+    r = '*'*l
+    return text.replace(word, r)
+
+print censor('What the flip is wrong?', 'flip')
